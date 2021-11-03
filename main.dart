12648909289e69251c56198a5348acfc96b52c58
ermekadiev94 <ermash.adiev@gmail.com>
2021-11-03 18:40:37 +0600

    sdfsdf

--- main.dart
+++ main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 void main() {
 //Lesson 7 Задание 1
   // String a = 'abcde';
@@ -66,22 +68,86 @@ void main() {
   // c == f ? {print('Да')} : print('Нет');
 
 // Lesson 7 Задание 5
-  var color = 'красный';
-  switch (color) {
-    case 'красный':
-      {
-        print('надо стоять');
-        break;
-      }
-    case 'желтый':
-      {
-        print('приготовиться');
-        break;
-      }
-    case 'зеленый':
-      {
-        print('можно идти');
-        break;
-      }
-  }
+  // var color = 'красный';
+  // switch (color) {
+  //   case 'красный':
+  //     {
+  //       print('надо стоять');
+  //       break;
+  //     }
+  //   case 'желтый':
+  //     {
+  //       print('приготовиться');
+  //       break;
+  //     }
+  //   case 'зеленый':
+  //     {
+  //       print('можно идти');
+  //       break;
+  //     }
+  // }
+
+  //Задание 1 (Wile)
+
+  // String city = 'Ankara';
+  // String a = 'a';
+  // List b = city.split('');
+  // List c = [];
+  // int i = 0;
+  // while (i < b.length) {
+  //   if (b[i] == a) {
+  //     c.add(b[i]);
+  //   }
+  //   i++;
+  // }
+  // print(c.length);
+
+  //Задание 2 (Wile)
+  // int? c;
+  // List a = [];
+  // var sum;
+  // do {
+  //   print('Число');
+  //   c = int.parse(stdin.readLineSync()!);
+  //   a.add(c);
+  // } while (c != 0);
+  // {
+  //   a.removeLast();
+  // }
+  // print(a.length);
+  // print(sum = a.reduce((value, element) => value + element));
+  // print(sum / a.length);
+
+  //Задание 3 (While)
+
+  // int? c;
+  // List a = [];
+  // var sum;
+  // do {
+  //   print('Число');
+  //   c = int.parse(stdin.readLineSync()!);
+  //   a.add(c);
+  // } while (c != 0);
+  // {
+  //   a.removeLast();
+  // }
+  // for (int i = 0; i < a.length; i++) {
+  //   if (a[i] % 2 == 0) {
+  //     a.remove(a[i]);
+  //   }
+  // }
+  // sum = a.reduce((value, element) => value + element);
+  // print(a);
+  // print(sum);
+
+  //Задание 1 (Напишите функцию)
+//   int min = 10;
+//   time(min);
+//   print('in $min  minutes ${time(10)} seconds');
+// }
+
+// time(int number) {
+//   var result;
+//   result = number * 60;
+//   return result;
 }
